758bc236dd4f2cdc2b079eafa6a9d3c308d2c8f8
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-07-03 10:43:32 +0100

    added trades folder

---
+++ necchange/app/trades/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function StudentSchedule() {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      This is a page created to display the Student Schedule
+    </main>
+  )
+}
