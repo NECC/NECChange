3aa39f2e636d5682176cffab13f746ea40fa530e
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2023-07-05 18:26:08 +0100

    api route to get classes (#10)

--- necchange/app/components/trades/schedule/StudentSchedule.jsx
+++ necchange/app/components/trades/schedule/StudentSchedule.jsx
@@ -1,12 +1,13 @@
 'use client'
 
 import React from 'react'
+
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
 
-export default function StudentSchedule({student_nr}) {
+export default function StudentSchedule({events}) {
     const minDate = new Date();
     minDate.setHours(8,0,0);
 
@@ -25,20 +26,8 @@ export default function StudentSchedule({student_nr}) {
             }}
             slotMinTime="08:00:00" // Set the minimum time to 8am
             slotMaxTime="20:00:00" // Set the maximum time to 8pm      
-            events={[
-                { 
-                    title: 'Lançamento NECChange', 
-                    start: '2023-07-03T09:00',
-                    end: '2023-07-03T11:00'
-                },
-                { 
-                    title: 'Começo das Aulas', 
-                    start: '2023-07-05T15:00',
-                    end: '2023-07-05T17:00'
-                }
-            ]}
+            events={events}
             height={'80vh'}
-            
         />
     )
 }
