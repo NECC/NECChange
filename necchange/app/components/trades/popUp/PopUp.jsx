e422499b96cba07baeb884b4dc037328b2a9b46d
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2023-07-06 16:14:34 +0100

    Trades (#14)
    * added trades folder
    
    * setup full calendar (#4)
    
    * api route to get classes (#10)
    
    * trocas bonitas (#5)
    
    * FullCalendar styles
    
    * blue
    
    * navbar
    
    * calendar styles
    
    * Calendar responsive and popUp interactivity
    
    * Calendar dayTimeGrid for 640px <
    
    * Style update to TailwindCSS
    
    ---------
    
    Co-authored-by: carlod035 <carlosdf035@gmail.com>
    Co-authored-by: David Machado <machado.david190@gmail.com>
    Co-authored-by: Quantik <40385569+quantik-git@users.noreply.github.com>
    Co-authored-by: Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
    
    * fixed merge issues
    
    * fixed some code
    
    * navbar & calendar fixes
    
    ---------
    
    Co-authored-by: Pedro Augusto <98715404+pedroacamargo@users.noreply.github.com>
    Co-authored-by: carlod035 <carlosdf035@gmail.com>
    Co-authored-by: David Machado <machado.david190@gmail.com>
    Co-authored-by: Quantik <40385569+quantik-git@users.noreply.github.com>
    Co-authored-by: pedroacamargo <pedroaugennes@gmail.com>

--- necchange/app/components/trades/popUp/PopUp.jsx
+++ necchange/app/components/trades/popUp/PopUp.jsx
@@ -1,12 +1,21 @@
+<<<<<<< HEAD
 'use client'
 
 import React from "react"
 
 export default function PopUp({ handleTradesPopUp, isTradesOpened }) {
+=======
+'use client' 
+
+import React from "react"
+
+export default function PopUp({handleTradesPopUp, isTradesOpened}) {
+>>>>>>> bd9e3a4 (Trades (#14))
     return (
         <div className={`w-11/12 h-5/6 z-[51] absolute bg-slate-100 bg-opacity-95 rounded-2xl left-[4.5%] top-24 ${isTradesOpened ? 'absolute' : 'hidden'}`}>
 
             <button onClick={handleTradesPopUp} className={`text-red-600 text-2xl absolute right-0 p-1 px-3 m-3 mr-4 rounded-full border border-red-600 hover:text-white hover:bg-red-600 hover:border-white cursor-pointer transition-all`}>X</button>
+<<<<<<< HEAD
 
             <div className="flex justify-center mt-24">
                 <div className="flex-grow m-12">
@@ -40,5 +49,9 @@ export default function PopUp({ handleTradesPopUp, isTradesOpened }) {
         </div>
 
 
+=======
+            
+        </div>
+>>>>>>> bd9e3a4 (Trades (#14))
     );
 }
