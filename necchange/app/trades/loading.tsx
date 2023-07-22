01a703fc36ac16dfdaa5a95e5d3de9c51b7d1f30
pedroacamargo <pedroaugennes@gmail.com>
2023-07-22 17:54:29 +0100

    loading white background

--- necchange/app/trades/loading.tsx
+++ necchange/app/trades/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex justify-center items-center h-screen bg-white">
             <div className="border-t-4 border-blue-500 border-solid rounded-full w-12 h-12 animate-spin"></div>
         </div>
     );
