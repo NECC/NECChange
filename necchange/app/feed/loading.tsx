07396a13c80d7c8884d77ab3815c93af0224d961
carlod035 <carlosdf035@gmail.com>
2023-07-22 11:48:51 +0100

    loading

---
+++ necchange/app/feed/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="border-t-4 border-blue-500 border-solid rounded-full w-12 h-12 animate-spin"></div>
+        </div>
+    );
+}
