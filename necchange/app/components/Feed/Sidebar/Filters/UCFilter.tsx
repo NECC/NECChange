bc0f56aa88e72b358538e7980189dfe590967342
Pedro Fernandes <63058328+pedromiguelf2001@users.noreply.github.com>
2023-08-08 15:01:56 +0100

    Feed working with filters! (#44)
    * Feed working with filters!
    
    * Update .env
    
    * removed unused years
    
    ---------
    
    Co-authored-by: SimaoQuintela <simaopedroquintela@hotmail.com>
    Co-authored-by: Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>

--- necchange/app/components/Feed/Sidebar/Filters/UCFilter.tsx
+++ necchange/app/components/Feed/Sidebar/Filters/UCFilter.tsx
@@ -1,10 +1,9 @@
-import { UnidadesCurricularesI } from "@/app/feed/interface";
 import { useState, Dispatch, SetStateAction } from "react";
 import { FiChevronDown } from "react-icons/fi";
 
 interface UcsFilterProps {
-    setUcsFilter: Dispatch<SetStateAction<UnidadesCurricularesI>>,
-    ucsFilter: UnidadesCurricularesI,
+    setUcsFilter: Dispatch<SetStateAction<string[]>>,
+    ucsFilter: string[],
     ucsArray: string[],
 }
 
@@ -13,40 +12,33 @@ export default function UCFilter(props: UcsFilterProps) {
     const {
         ucsArray,
         ucsFilter,
-        setUcsFilter
+        setUcsFilter,
     } = props;
 
     const [isVisible, setIsVisible] = useState(false);
     const toggleIsVisible = () => setIsVisible(!isVisible);
 
     // function to change the filter state
-    const handleFilter = (e: any) => {
-        const newFilter = {
+    const handleFilter = (e: any) => {    
+        if(ucsFilter.indexOf(e.target.id) == -1){
+        const newFilter: string[] = [
             ...ucsFilter,
-            [e.target.id]: e.target.checked,
-        }
+            e.target.id,
+        ]
         setUcsFilter(newFilter);
+        }
+        else{
+            const newFilter = ucsFilter.filter((uc: any) => {                
+                return uc != e.target.id
+            })
+            setUcsFilter(newFilter);
+        }
     }
     
-
-    // This little code snippet will transform a portuguese phrase in a concatenated with each word capitalized string
-    // without accents and ç
-    // Álgebra Universal e Categorias = AlgebraUniversalECategorias
-    const arraySanitization = (array: string[]): string[] => (
-        array.map((item: string) => ((item
-                        .split(" ")
-                        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-                        .join(" "))
-                        .replace(/[,\s]/g, ""))
-                        .normalize("NFD")
-                        .replace(/[\u0300-\u036f]/g, "")
-        )
-    )
-
     return (
-        <div className="border-b pb-3"> 
+        <div className="border-b border-t pb-3 pt-3"> 
             <div className="flex items-center justify-between" onClick={toggleIsVisible}>
-                <strong className="text-lg cursor-pointer">Unidade Curricular</strong>
+                <strong className="text-md ml-7 cursor-pointer">Unidade Curricular</strong>
                 <span className="ml-2 cursor-pointer">
                     <FiChevronDown size={24} className={`transition-all duration-300 ${isVisible ? "rotate-180" : "rotate-0"}`}/>
                 </span>
@@ -56,9 +48,9 @@ export default function UCFilter(props: UcsFilterProps) {
                 <form className="mt-2 accent-blue-500 ">
                     {
                         ucsArray.map((uc, index) => (
-                            <div key={index}>
-                                <input onClick={handleFilter} type="checkbox" id={`${arraySanitization(ucsArray)[index]}`} name="uc" value={`${arraySanitization(ucsArray)[index]}`} className="mb-2"/>
-                                <label htmlFor={`${arraySanitization(ucsArray)[index]}`} className="mr-2 "> {uc} </label>
+                            <div className="ml-7" key={index}>
+                                <input onChange={handleFilter} type="checkbox" id={`${ucsArray[index]}`} name="uc" value={`${ucsArray[index]}`} className="mb-2"/>
+                                <label htmlFor={`${ucsArray[index]}`} className="mr-2 "> {uc} </label>
                             </div>
                         ))
                     }
