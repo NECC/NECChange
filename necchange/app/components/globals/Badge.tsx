7e7fa61efa1477aeb34d75fd0bb072576ed88c3e
David Machado <machado.david190@gmail.com>
2023-08-26 12:53:43 +0100

    Feed and Calendar refactor

---
+++ necchange/app/components/globals/Badge.tsx
@@ -0,0 +1,24 @@
+interface BadgeProps {
+  variant: "green" | "yellow" | "red";
+  children: any;
+}
+
+const Badge = ({ variant, children }: BadgeProps) => {
+  const variants = {
+    green: ["text-green-800 bg-green-100", "bg-green-500"],
+    yellow: ["text-yellow-800 bg-yellow-100", "bg-yellow-500"],
+    red: ["text-red-800 bg-red-100", "bg-red-500"],
+  };
+
+  const [textColor, bgColor] = variants[variant];
+  return (
+    <span
+      className={`flex items-center text-xs font-semibold rounded-full py-1 px-2 ${textColor}`}
+    >
+      <div className={`h-[6px] w-[6px] rounded-full mr-1 ${bgColor}`} />
+      {children}
+    </span>
+  );
+};
+
+export default Badge;
