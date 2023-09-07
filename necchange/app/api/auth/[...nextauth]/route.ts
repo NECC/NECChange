ea6dd92d9cb8ddf48ded8f7cc3d3552d905db376
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-07 12:29:45 +0100

    Changed auth api route

--- necchange/app/api/auth/[...nextauth]/route.ts
+++ necchange/app/api/auth/[...nextauth]/route.ts
@@ -1,44 +1,5 @@
 import NextAuth from 'next-auth'
-import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient, Role } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export const authOptions: any = {
-  adapter: PrismaAdapter(prisma),
-
-  pages: {
-    signIn: "/auth/signin",
-    signOut: "/"
-  },
-  providers: [
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-      }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  debug: true,
-
-  callbacks: {
-    async jwt({token, user}: any){
-      return {...token, ...user};
-    },
-    
-    async session({ session, token }: any) {
-      // Add role to session provided from useSession
-      session.user.role = token.role
-      session.user.number = token.number
-
-      return session
-    },   
-  },
-  session: { strategy: "jwt" },
-  events: {
-  }
-
-}
+import { authOptions } from '@/app/lib/authOptions';
 
 const handler = NextAuth(authOptions)
 
---
+++ necchange/app/lib/authOptions.ts
@@ -0,0 +1,40 @@
+import { NextAuthOptions } from 'next-auth'
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+  
+    pages: {
+      signIn: "/auth/signin",
+      signOut: "/"
+    },
+    providers: [
+      EmailProvider({
+        server: process.env.EMAIL_SERVER,
+        from: process.env.EMAIL_FROM,
+        }),
+    ],
+    secret: process.env.NEXTAUTH_SECRET,
+    debug: true,
+  
+    callbacks: {
+      async jwt({token, user}: any){
+        return {...token, ...user};
+      },
+      
+      async session({ session, token }: any) {
+        // Add role to session provided from useSession
+        session.user.role = token.role
+        session.user.number = token.number
+  
+        return session
+      },   
+    },
+    session: { strategy: "jwt" },
+    events: {
+    }
+  
+  }
