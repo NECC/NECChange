b560872e37025f3d374d061947c34cbd42f78bcf
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-12 18:33:10 +0100

    fixed invalid route type

--- necchange/app/api/auth/[...nextauth]/route.ts
+++ necchange/app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,6 @@
-import NextAuth from 'next-auth/next'
+import NextAuth from 'next-auth'
 import { authOptions } from '@/app/lib/authOptions';
-import { NextApiRequest, NextApiResponse } from 'next';
 
-//const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions)
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-    if(req.url && req.method === "HEAD") {
-        return res.status(200).end()
-    }
-
-    return NextAuth(req, res, authOptions)
-}
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    if(req.url && req.method === "HEAD") {
-        return res.status(200).end()
-    }
-
-    return NextAuth(req, res, authOptions)
-}
-
-//export { auth as POST }
+export { handler as GET, handler as POST }
