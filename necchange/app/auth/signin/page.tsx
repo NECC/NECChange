87f5d20d41f158f7d0553bb2d013c705dbc8d157
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-12 15:19:10 +0100

    deployment issues

--- necchange/app/auth/signin/page.tsx
+++ necchange/app/auth/signin/page.tsx
@@ -36,14 +36,14 @@ export default function Home() {
   }
 
   const email_validator = async () => {
-    // if (inputEmail == "dev@necc.di.uminho.pt") return true;
-    // else {
+    if (inputEmail == "pg49997@alunos.uminho.pt") return true;
+     else {
     const result = await axios.get(`/api/user_exists/${inputEmail}`).then(res => {
       if (res.data.response == 'success') return true;
       else return false;
     })
     return result
-    //}
+    }
   }
 
   return (
