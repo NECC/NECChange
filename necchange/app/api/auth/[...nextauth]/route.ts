3dea96b88ff15e803568315b532bff8725d2c157
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-12 20:38:12 +0100

    added HEAD method

--- necchange/app/api/auth/[...nextauth]/route.ts
+++ necchange/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
 import NextAuth from 'next-auth'
 import { authOptions } from '@/app/lib/authOptions';
 
+import { NextResponse } from 'next/server'
+
 const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+const fuckAll = NextResponse.json({}, {status: 200})
+
+export { handler as GET, handler as POST, fuckAll as HEAD }
