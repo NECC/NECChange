6ec61db0f26c61ee7733767c4d0091b4390ff37d
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-12 19:31:40 +0100

    modified middleware

--- necchange/middleware.ts
+++ necchange/middleware.ts
@@ -3,23 +3,25 @@ import { adminRoutes, authRoutes, protectedRoutes } from "./app/routes/routes";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-    const token = await getToken({req: request});
-    //console.log('My token middleware:', token);
-    
+    const token = await getToken({ req: request });
+    const userAgent = request.headers.get('user-agent');
+
+    // Block HEAD requests from Outlook
+    if (request.method === 'HEAD' && userAgent && userAgent.includes('Outlook')) {
+        return new Response(null, { status: 200 }); // Return a 403 Forbidden response
+    }
+
     // Admin paths
-    // if signed in and doesn't have role SUPER_USER, can't access /super_user/**
     if (adminRoutes.some((path) => request.nextUrl.pathname.startsWith(path))) {
         if (!token || token.role != 'SUPER_USER') return NextResponse.redirect(new URL('/', request.url));
     }
 
     // Protected paths
-    // if not signed in, user can't access /horario and /feed
     if (protectedRoutes.some((path) => request.nextUrl.pathname.startsWith(path))) {
         if (!token) return NextResponse.redirect(new URL('/', request.url));
     }
-    
+
     // Auth paths
-    // if signed in, user can't access /auth paths
     if (authRoutes.some((path) => request.nextUrl.pathname.startsWith(path))) {
         if (token) return NextResponse.redirect(new URL('/', request.url));
     }
