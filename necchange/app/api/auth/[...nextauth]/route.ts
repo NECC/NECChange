6d7b4146dcfa785a0d85abce0089441f6fccb5c3
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-12 16:31:36 +0100

    fixed safe link problem

--- necchange/app/api/auth/[...nextauth]/route.ts
+++ necchange/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,15 @@
 import NextAuth from 'next-auth'
 import { authOptions } from '@/app/lib/authOptions';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-const handler = NextAuth(authOptions)
+//const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST }
+export async function auth(req: NextApiRequest, res: NextApiResponse) {
+    if(req.method === "HEAD") {
+        return res.status(200).end()
+     }
+
+     return NextAuth(req, res, authOptions)
+  }
+
+export { auth as GET, auth as POST }
