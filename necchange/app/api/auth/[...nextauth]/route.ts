ccf9c309b16e1007b404108d175904c7815ad274
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-12 20:50:30 +0100

    mais um

--- necchange/app/api/auth/[...nextauth]/route.ts
+++ necchange/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,11 @@
 import NextAuth from 'next-auth'
 import { authOptions } from '@/app/lib/authOptions';
-
-import { NextResponse } from 'next/server'
+import { NextResponse, NextRequest } from 'next/server'
 
 const handler = NextAuth(authOptions)
 
-const fuckAll = () => NextResponse.json({}, {status: 200})
+export async function HEAD(req: NextRequest, context: any) {
+  return NextResponse.json({}, {status: 200})
+}
 
-export { handler as GET, handler as POST, fuckAll as HEAD }
+export { handler as GET, handler as POST }
