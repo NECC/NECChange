fb649359aacee3f214c78aaed7a8fceead247f7d
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-13 19:39:13 +0100

    x

--- necchange/app/api/auth/[...nextauth]/route.ts
+++ necchange/app/api/auth/[...nextauth]/route.ts
@@ -3,13 +3,11 @@ import { authOptions } from "@/app/lib/authOptions";
 import { NextResponse } from "next/server";
 
 const handler = async (req: any, context: { params: any }) => {
-  console.log("handler", req.method);
-  //const domain = req.headers.get('host')
-
-  //console.log(domain);
+  console.log("handler", req.method, context);
+  // const domain = req.headers.get('host')
 
   if(req.method == 'HEAD'){
-    return NextResponse.json({status: 200})
+    return NextResponse.json({}, {status: 200})
   }
 
   return await NextAuth(req, context, authOptions);
