dda41c32f8b55d5177e917569ddca2e72a2bb7b9
David Machado <machado.david190@gmail.com>
2023-09-14 11:47:15 +0100

    Update route.ts

--- necchange/app/api/auth/[...nextauth]/route.ts
+++ necchange/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,10 @@
-import NextAuth from 'next-auth'
-import { authOptions } from '@/app/lib/authOptions';
+import NextAuth from "next-auth";
+import { authOptions } from "@/app/lib/authOptions";
 
-const handler = NextAuth(authOptions)
+const handler = async (req: any, context: { params: any }) => {
+  console.log("handler\n", req.method, req.headers, "\n\n");
 
-export { handler as GET, handler as POST }
+  return await NextAuth(req, context, authOptions);
+};
+
+export { handler as GET, handler as POST, handler as HEAD };
