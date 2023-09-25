40be6b6017b9e9786ac443767d50013f293c6f2d
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-09-25 19:38:23 +0100

    Fixed login in production and optimized navbar

--- necchange/app/components/Provider.jsx
+++ necchange/app/components/Provider.jsx
@@ -1,7 +1,6 @@
-'use client'
+"use client"
 
 import { SessionProvider } from 'next-auth/react'
-
 const Provider = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>
 }
--- necchange/app/components/globals/navbar/MobileNavbarLinks.tsx
+++ necchange/app/components/globals/navbar/MobileNavbarLinks.tsx
@@ -3,14 +3,15 @@ import NavbarLinks from "./NavbarLinks";
 
 interface MobileNavbarLinksProps {
     isMenuOpened: boolean,
+    session: any
 }
 
 function MobileNavbarLinks(props: MobileNavbarLinksProps) {
-    const { isMenuOpened } = props;
+    const { isMenuOpened, session } = props;
 
     return (
         <div className={`${isMenuOpened ? "" : "hidden"} w-full sm:hidden block`}>
-            <NavbarLinks containerDinamicClass={`pb-4 flex flex-col align-start`}/>          
+            <NavbarLinks containerDinamicClass={`pb-4 flex flex-col align-start`} session={session}/>          
         </div>
     );
 }
