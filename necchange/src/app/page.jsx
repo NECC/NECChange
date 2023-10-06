3847f991af5395bdcc961834238d1388d7856165
pedroacamargo <pedroaugennes@gmail.com>
2023-10-07 00:02:04 +0100

    Year filter done

--- necchange/src/app/page.jsx
+++ necchange/src/app/page.jsx
@@ -8,12 +8,15 @@ import { useEffect, useState } from "react";
 
 export default function CalendarPage() {
   const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
+  const [yearFilter, setYearFilter] = useState([]);
   const [currentSeason, setCurrentSeason] = useState('2023/2024');
+  // const [UCs, setUCs] = useState([]);
 
   useEffect(() => {
     axios.get("/api/calendar/getCalendar").then((res) => {
       
-      // Function that filters 2023/2024 events
+      // Function that filters 2023/2024 events (Should be deleted later, it's just to clean old events in the development)
       const ucsCurrentSeason = res.data.response.reduce((acc, uc) => {
 
         const season = currentSeason.split('/').map((year) => Number(year));
@@ -34,9 +37,40 @@ export default function CalendarPage() {
       });
       
       setEvents(eventsFinished);
+      setFilteredEvents(eventsFinished);
       console.log(eventsFinished);
     });
   }, []);
+  
+  useEffect(() => {
+    if (yearFilter.length != 0) {
+      const newEvents = events.filter((elem) => {
+        return yearFilter.includes(elem.year) || elem.year == 0;
+      })
+      setFilteredEvents(newEvents);
+    } else {
+      setFilteredEvents(events);
+    }
+  }, [yearFilter]);
+
+  // useEffect(() => {
+  //   axios.get("/api/calendar/getUCS").then((res) => {
+  //     setUCs(res.data.response);
+  //     // console.log("UCS: " ,res.data.response);
+  //   })
+  // }, []);
+
+  const handleYear = (e) => {
+    const year = Number((e.target.id).charAt(0));
+
+    if (yearFilter.includes(year)) {
+      const newArray = yearFilter.filter((y) => y != year);
+      setYearFilter(newArray);
+    } else {
+      const newArray = [...yearFilter, year];
+      setYearFilter(newArray);
+    }
+  }
 
   const getYearByEvent = (event) => {
     const regex = /\((1|2|3)º ano\)/
@@ -47,16 +81,15 @@ export default function CalendarPage() {
       return year;
     } else return 0;
   }
-  
-  // useEffect(() => {
-  //   axios.get("/api/calendar/getUCS").then((res) => {
-  //     setUCs(res.data.response);
-  //     console.log("UCS: " ,res.data.response);
-  //   })
-  // }, []);
 
   return (
     <div className="bg-white min-h-screen pt-24">
+      <input onChange={handleYear} type="checkbox" name="1ano" id="1ano" />
+      <label htmlFor="1ano">1° ano</label>
+      <input onChange={handleYear} type="checkbox" name="2ano" id="2ano" />
+      <label htmlFor="2ano">2° ano</label>
+      <input onChange={handleYear} type="checkbox" name="3ano" id="3ano" />
+      <label htmlFor="3ano">3° ano</label>
       <div className="pt-8 px-8 overflow-y-scroll full-calendar calendar-container container mx-auto">
         <FullCalendar
           plugins={[dayGridPlugin, googleCalendarPlugin]}
@@ -69,7 +102,7 @@ export default function CalendarPage() {
           }}
           initialView="dayGridMonth"
           displayEventTime={false}
-          events={events}
+          events={filteredEvents}
           eventColor="blue-sky-500"
           height="80vh"
         />
