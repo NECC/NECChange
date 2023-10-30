af102f88467720acca2e90273ebc3904e8ba2d9b
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-10-30 19:13:29 +0000

    add packages

--- necchange/src/lib/authOptions.js
+++ necchange/src/lib/authOptions.js
@@ -58,6 +58,7 @@ async function sendVerificationRequest(params) {
   }
 }
 
+
 /**
  * Email HTML body
  * Insert invisible space into domains from being turned into a hyperlink by email
@@ -81,19 +82,15 @@ function html(params) {
     buttonText: theme.buttonText || "#fff",
   };
 
-  const redirect = process.env.NEXTAUTH_URL + "/auth/captcha?redirect=" + url
+  const redirect = process.env.NEXTAUTH_URL + "/auth/captcha?redirect=" + url;
 
   return `
 <body style="background: ${color.background};">
   <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: ${
-      color.mainBackground
-    }; max-width: 600px; margin: auto; border-radius: 10px;">
+    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
     <tr>
       <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${
-          color.text
-        };">
+        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
         Sign in to <strong>${escapedHost}</strong>
       </td>
     </tr>
@@ -101,15 +98,9 @@ function html(params) {
       <td align="center" style="padding: 20px 0;">
         <table border="0" cellspacing="0" cellpadding="0">
           <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${
-              color.buttonBackground
-            }"><a href="${redirect}"
+            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${redirect}"
                 target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${
-                  color.buttonText
-                }; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${
-                  color.buttonBorder
-                }; display: inline-block; font-weight: bold;">Sign
+                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
                 in</a></td>
           </tr>
         </table>
@@ -117,9 +108,8 @@ function html(params) {
     </tr>
     <tr>
       <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${
-          color.text
-        };">
+        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+        <a href="${redirect}">Sign in here</a>
         If you did not request this email you can safely ignore it.
         <br>
       </td>
