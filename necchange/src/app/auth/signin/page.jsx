a6af09d61189ca10213dc4b4a609e8ccef18fa3d
SimaoQuintela <simaopedroquintela@hotmail.com>
2023-11-12 00:05:07 +0000

    Added placeholder to email

--- necchange/src/app/auth/signin/page.jsx
+++ necchange/src/app/auth/signin/page.jsx
@@ -66,11 +66,11 @@ export default function Home() {
                       id="email"
                       name="email"
                       type="email"
-                      placeholder="Email Institucional"
+                      placeholder="aXXXXX@alunos.uminho.pt"
                       autoComplete="email"
                       required
                       className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      onChange={(e) => setInputEmail(e.target.value)}
+                      onChange={(e) => setInputEmail(e.target.value.toLowerCase())}
                     />
                   </div>
                   {errorMessage && (
