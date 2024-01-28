dd01dd4c8105b713fd6431ab58e6d82d816b5aca
SimaoQuintela <simaopedroquintela@hotmail.com>
2024-01-28 15:30:38 +0000

    chore: update middleware - route access based on role

--- necchange/src/middleware.js
+++ necchange/src/middleware.js
@@ -12,20 +12,20 @@ import { getToken } from "next-auth/jwt";
  */
 const publicRoutes = ["/"];
 const authRoutes = ["/auth"];
-// for now, horario and feed are protected routes. When necchange is full released, change that routes to protected 
-const adminRoutes = ["/super_user", "/partner", ];
+// for now, horario and feed are protected routes. When necchange is full released, change that routes to protected
+const adminRoutes = ["/super_user"];
 const protectedRoutes = ["/profile", "/horario", "/feed"];
 
 export async function middleware(request) {
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
-    cookieName: process.env.NODE_ENV === 'production' ? '__Secure-next-auth.session-token' : 'next-auth.session-token'
+    cookieName:
+      process.env.NODE_ENV === "production"
+        ? "__Secure-next-auth.session-token"
+        : "next-auth.session-token",
   });
 
-  //console.log('Request', request);
-  //console.log("My token middleware:", token);
-  //console.log("path", request.nextUrl.pathname);
   // Admin paths
   // if signed in and doesn't have role SUPER_USER, can't access /super_user/**
   if (adminRoutes.some((path) => request.nextUrl.pathname.startsWith(path))) {
@@ -35,11 +35,14 @@ export async function middleware(request) {
 
   // Protected paths
   // if not signed in, user can't access /horario and /feed
-  
   if (
     protectedRoutes.some((path) => request.nextUrl.pathname.startsWith(path))
   ) {
     if (!token) return NextResponse.redirect(new URL("/", request.url));
+
+    /* Some edge cases */
+    if(request.nextUrl.pathname.startsWith("/profile") && token.partner == false) return NextResponse.redirect(new URL("/", request.url));
+    if(request.nextUrl.pathname.startsWith("/feed") && (token.role == "OUTSIDER")) return NextResponse.redirect(new URL("/", request.url));
   }
 
   // Auth paths
