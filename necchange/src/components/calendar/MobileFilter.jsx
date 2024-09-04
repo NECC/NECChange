5102c35573c1eeedbdfccb97d62357625abdcbb1
Edgar Araújo <edgararaj@gmail.com>
2024-09-04 14:16:21 +0100

    Alternar botao filtro mobile de filtrar para fechar

--- necchange/src/components/calendar/MobileFilter.jsx
+++ necchange/src/components/calendar/MobileFilter.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import CheckboxTree from './CheckboxTree/CheckboxTree';
 import { Button } from "@nextui-org/react";
 import FilterIcon from '../FilterIcon';
+import { IoClose } from "react-icons/io5";
 
 export default function MobileFilter(props) {
     const { className, nodes, checked, onCheck } = props;
@@ -11,8 +12,8 @@ export default function MobileFilter(props) {
 
     return (
         <>
-            <Button onClick={() => setIsOpened(!isOpened)} color="primary" size="lg" startContent={<FilterIcon/>} className="absolute bottom-12 right-6 z-50 lg:hidden">
-                Filtrar
+            <Button onClick={() => setIsOpened(!isOpened)} color="primary" size="lg" startContent={!isOpened ? <FilterIcon /> : <IoClose className="w-6 h-6" />} className="absolute bottom-12 right-6 z-50 lg:hidden">
+                {!isOpened ? 'Filtrar' : 'Fechar'}
             </Button>
             <div className={`absolute w-full h-full transition-all duration-500 ${isOpened ? 'left-0' : '-left-[100%]'} top-0 z-10 bg-white/90 flex justify-center items-start mt-16 mb-24 overflow-auto`}>
                 <CheckboxTree
